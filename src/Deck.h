5f2f516d4da6719e6113b5828f0fc790432dcc03
Abrams <leadersia5566@gmail.com>
2025-10-30 19:28:07 +0200

    Moving files

---
+++ src/Deck.h
@@ -0,0 +1,14 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include "Card.h"
+#include <vector>
+
+class Deck {
+    std::vector<Card> m_cards;
+public:
+    Deck();             
+    void shuffle();     
+    Card deal();        
+
+#endif
